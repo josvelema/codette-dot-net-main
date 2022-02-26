97f4b8d7b6569dc90a4aa18435ea17d170e375c1
Joseph R <49407831+josvelema@users.noreply.github.com>
2022-02-26 17:22:48 +0100

    initial commit , coming soon page created

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Codette dot Net | Home</title>
+</head>
+<body>
+  <main>
+    <header>
+      <nav></nav>
+      <section>
+        <article>
+          <div class="cta">
+            <p style="text-align: center; font-size: 2rem;">Coming soon</p>
+          </div>
+
+        </article>
+      </section>
+    </header>
+  </main>
+  <hr>
+  <footer>
+    <p>Created by Jos - <a href="mailto:jos@codette.net">jos@codette.net</a></p>
+  </footer>
+</body>
+</html>
